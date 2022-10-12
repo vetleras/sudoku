fa4529ff11edd322b31c14bd85e20e0bb07edfc5
vetleras <vetleras@gmail.com>
2022-10-12 16:17:05 +0200

    Use type alias instead of custom struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use anyhow::{bail, Context, Result};
-use std::{
-    collections::HashSet,
-    env, fmt, fs,
-    ops::{Index, IndexMut},
-};
+use std::{collections::HashSet, env, fs};
 
 type Domain = HashSet<u8>;
 
@@ -13,103 +9,44 @@ enum Variable {
     Unassigned(Domain),
 }
 
-#[derive(Clone, Debug)]
-struct Assignment([Variable; 81]);
+type Assignment = [Variable; 81];
 
-impl Assignment {
-    fn from_file(filepath: &str) -> Result<Self> {
-        let v = fs::read_to_string(filepath)?
-            .chars()
-            .filter(|c| !c.is_whitespace())
-            .map(|c| match c.to_digit(10) {
-                Some(0) => Ok(Variable::Unassigned((1..10).collect())),
-                Some(n) => Ok(Variable::Assigned(n as u8)),
-                None => bail!("non-digit in input"),
-            })
-            .collect::<Result<Vec<Variable>>>()?;
+fn assignment_from_file(filepath: &str) -> Result<Assignment> {
+    let v = fs::read_to_string(filepath)?
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| match c.to_digit(10) {
+            Some(0) => Ok(Variable::Unassigned((1..10).collect())),
+            Some(n) => Ok(Variable::Assigned(n as u8)),
+            None => bail!("non-digit in input"),
+        })
+        .collect::<Result<Vec<Variable>>>()?;
 
-        match v.try_into() {
-            Ok(array) => Ok(Assignment(array)),
-            Err(e) => bail!("invalid length of input ({})", e.len()),
-        }
+    match v.try_into() {
+        Ok(array) => Ok(array),
+        Err(e) => bail!("invalid length of input ({})", e.len()),
     }
+}
 
-    fn assigned_variables(&self) -> Vec<(usize, u8)> {
-        self.0
-            .iter()
-            .enumerate()
-            .filter_map(|(x, var)| match var {
-                Variable::Assigned(val) => Some((x, *val)),
-                Variable::Unassigned(_) => None,
-            })
-            .collect()
-    }
+fn assigned_variables(assignment: &Assignment) -> Vec<(usize, u8)> {
+    assignment
+        .iter()
+        .enumerate()
+        .filter_map(|(x, var)| match var {
+            Variable::Assigned(val) => Some((x, *val)),
+            Variable::Unassigned(_) => None,
+        })
+        .collect()
+}
 
-    fn unassigned_variable(&self) -> Option<(usize, &Domain)> {
-        self.0.iter().enumerate().find_map(|(x, var)| match var {
+fn unassigned_variable(assignment: &Assignment) -> Option<(usize, &Domain)> {
+    assignment
+        .iter()
+        .enumerate()
+        .find_map(|(x, var)| match var {
             Variable::Assigned(_) => None,
             Variable::Unassigned(d) => Some((x, d)),
         })
-    }
-}
-
-impl Index<usize> for Assignment {
-    type Output = Variable;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
-    }
-}
-
-impl IndexMut<usize> for Assignment {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.0[index]
-    }
-}
-
-impl fmt::Display for Assignment {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for (i, v) in self.0.iter().enumerate() {
-            if i % 9 == 0 && i != 0 {
-                f.write_str("\n")?;
-            }
-            if let Variable::Assigned(val) = v {
-                f.write_str(&val.to_string())?;
-            } else {
-                panic!("tried to print unassigned value {:?}", v);
-            }
-        }
-        Ok(())
-    }
-}
-
-fn backtrack(
-    assignment: Assignment,
-    mut called: i32,
-    mut failed: i32,
-) -> Result<(Assignment, i32, i32)> {
-    called += 1;
-    if let Some((x, domain)) = assignment.unassigned_variable() {
-        for val in domain {
-            let mut new_assignment = assignment.clone();
-            new_assignment[x] = Variable::Assigned(*val);
-            if let Ok(a) = ac3(new_assignment) {
-                new_assignment = a;
-            } else {
-                continue;
-            }
-
-            let result = backtrack(new_assignment, called, failed);
-            if result.is_ok() {
-                return result;
-            } else {
-                failed += 1;
-            }
-        }
-        bail!("failure")
-    } else {
-        Ok((assignment, called, failed))
-    }
 }
 
 fn generate_constraints(x: usize) -> Vec<usize> {
@@ -145,8 +82,37 @@ fn generate_constraints(x: usize) -> Vec<usize> {
     constraints
 }
 
+fn backtrack(
+    assignment: Assignment,
+    mut called: i32,
+    mut failed: i32,
+) -> Result<(Assignment, i32, i32)> {
+    called += 1;
+    if let Some((x, domain)) = unassigned_variable(&assignment) {
+        for val in domain {
+            let mut new_assignment = assignment.clone();
+            new_assignment[x] = Variable::Assigned(*val);
+            if let Ok(a) = ac3(new_assignment) {
+                new_assignment = a;
+            } else {
+                continue;
+            }
+
+            let result = backtrack(new_assignment, called, failed);
+            if result.is_ok() {
+                return result;
+            } else {
+                failed += 1;
+            }
+        }
+        bail!("failure")
+    } else {
+        Ok((assignment, called, failed))
+    }
+}
+
 fn ac3(mut assignment: Assignment) -> Result<Assignment> {
-    let mut queue = assignment.assigned_variables();
+    let mut queue = assigned_variables(&assignment);
     while let Some((x, val)) = queue.pop() {
         for y in generate_constraints(x) {
             match assignment[y] {
@@ -166,15 +132,30 @@ fn ac3(mut assignment: Assignment) -> Result<Assignment> {
     Ok(assignment)
 }
 
+fn print_assignment(assignment: &Assignment) {
+    for i in (0..81).step_by(9) {
+        let line = assignment[i..(i + 9)]
+            .iter()
+            .map(|v| match v {
+                Variable::Assigned(val) => val.to_string(),
+                Variable::Unassigned(domain) => {
+                    panic!("tried to print unassigned value {:?}", domain);
+                }
+            })
+            .collect::<String>();
+        println!("{line}");
+    }
+}
+
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let filepath = args.get(1).context("filepath missing as cmd arg")?;
-    let assignment = Assignment::from_file(filepath)
-        .with_context(|| format!("loading assignment from file"))?;
+    let assignment =
+        assignment_from_file(filepath).with_context(|| format!("loading assignment from file"))?;
 
     let (solved_assignment, called, failed) = backtrack(assignment, 0, 0)?;
     println!("backtrack called: {called}");
     println!("backtrack failed: {failed}\n");
-    println!("{solved_assignment}");
+    print_assignment(&solved_assignment);
     Ok(())
 }
